087adc16f4364eb23717ab07115c812c1a96f534
Brad Coleman <bradcoleman60@gmail.com>
2023-01-20 07:07:19 -0800

    adds Map objection function and tech iterator function

--- TEST_README.md
+++ TEST_README.md
@@ -1,15 +1,7 @@
-The title of this project is REadmen and the author is Brad. 
+The title of this project is sss and the author is ssss. 
 
-The description of this project is: This is it.
+The description of this project is: sss.
     
-The technology used in this project included: JavaScript,HTML,CSS,NODE.js,Jquery
+The technology used in this project included: JavaScript,HTML,NODE.js'
 
-| Technology Used | Resource URL | 
-| ------------- |:-------------| 
-| Git | [https://git-scm.com/](https://git-scm.com/) |   
-| HTML | [https://developer.mozilla.org/en-US/docs/Web/HTML](https://developer.mozilla.org/en-US/docs/Web/HTML) | 
-| CSS | [https://developer.mozilla.org/en-US/docs/Web/CSS](https://developer.mozilla.org/en-US/docs/Web/CSS) |   
-| JavaScript | [https://developer.mozilla.org/en-US/docs/Learn/JavaScript](https://developer.mozilla.org/en-US/docs/Learn/JavaScript) |
-| UI Kit | [https://getuikit.com/docs/introduction](https://getuikit.com/docs/introduction) |
-| jQuery API | [https://api.jquery.com/](https://api.jquery.com/) |
-| Web APIs | [https://developer.mozilla.org/en-US/docs/Web/API](https://developer.mozilla.org/en-US/docs/Web/API) |
+
--- index.js
+++ index.js
@@ -48,35 +48,54 @@ inquirer
     console.info("answer:", answers.author);
     console.info("answer:", answers.description);
     console.info("answer:", answers.technology);
-    textContent(
+    settextContent(
       projectInputs.title,
       projectInputs.author,
       projectInputs.description,
       projectInputs.technology
     );
+    techIterator(projectInputs.technology)
   });
 
-function textContent(a, b, c, d) {
-var theReadMe = 
-`The title of this project is ${a} and the author is ${b}. 
+/* This sets the content (via a template literal string) of the 
+readme document*/
+function settextContent(a, b, c, d) {
+  var theReadMe = `The title of this project is ${a} and the author is ${b}. 
 
 The description of this project is: ${c}.
     
-The technology used in this project included: ${d}
+The technology used in this project included: ${d}'
 
-| Technology Used | Resource URL | 
-| ------------- |:-------------| 
-| Git | [https://git-scm.com/](https://git-scm.com/) |   
-| HTML | [https://developer.mozilla.org/en-US/docs/Web/HTML](https://developer.mozilla.org/en-US/docs/Web/HTML) | 
-| CSS | [https://developer.mozilla.org/en-US/docs/Web/CSS](https://developer.mozilla.org/en-US/docs/Web/CSS) |   
-| JavaScript | [https://developer.mozilla.org/en-US/docs/Learn/JavaScript](https://developer.mozilla.org/en-US/docs/Learn/JavaScript) |
-| UI Kit | [https://getuikit.com/docs/introduction](https://getuikit.com/docs/introduction) |
-| jQuery API | [https://api.jquery.com/](https://api.jquery.com/) |
-| Web APIs | [https://developer.mozilla.org/en-US/docs/Web/API](https://developer.mozilla.org/en-US/docs/Web/API) |`;
 
-    fs.writeFile('TEST_README.md',theReadMe, (err) =>{
-        if (err) throw err;
-        console.log('The file is saved')
-    })
-    console.log(theReadMe)
+`;
+
+  writeReadme(theReadMe);
+}
+/*This function writes the readme document*/
+function writeReadme(theReadMe) {
+  fs.writeFile("TEST_README.md", theReadMe, (err) => {
+    if (err) throw err;
+    console.log("The file is saved");
+  });
+  console.log(theReadMe);
+  techList();
+}
+
+//This function adds ability to obtain which technologies were selected
+function techIterator (projTech){
+for(var i=0; i < projTech.length; i++){
+    console.log(projTech[i])
+}}
+
+
+/*This function gets the link to the technology used in the project */
+function techList() {
+  const techLookUp = new Map();
+  techLookUp.set("git", "[https://git-scm.com/](https://git-scm.com/)");
+  techLookUp.set(
+    "HTML",
+    "[https://developer.mozilla.org/en-US/docs/Web/HTML](https://developer.mozilla.org/en-US/docs/Web/HTML)"
+  );
+
+  console.log(techLookUp.get("git"));
 }
